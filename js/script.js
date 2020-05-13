3ce293dddfa9d2ea1fac5e11db2853502ff37d7b
Maciek Lubocki <mail2maciek@gmail.com>
2020-05-13 15:32:04 +0200

    Tag function add

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 {
     const optArticleSelector = '.post',
         optTitleSelector = '.post-title',
-        optTitleListSelector = '.titles';
+        optTitleListSelector = '.titles',
+        optArticleTagsSelector = '.post-tags .list';
+
     const titleClickHandler = function(event) {
         event.preventDefault();
         console.log('Link was clicked!');
@@ -45,7 +47,7 @@
         /* [done] add class 'active' to the correct article */
 
         targetArticle.classList.add('active');
-    }
+    };
 
     function generateTitleLinks() {
 
@@ -102,3 +104,67 @@
 
     generateTitleLinks();
 }
+
+
+function generateTags() {
+    /* find all articles */
+
+    const articles = document.querySelectorAll(optArticleTagsSelector);
+    console.log('Articles: ', articles);
+
+
+
+    /* START LOOP: for every article: */
+
+    for (let article of articles) {
+        console.log(' Article ELement is: ', article);
+
+        /* find tags wrapper */
+
+
+
+
+
+
+
+
+        /* make html variable with empty string */
+        let html = '';
+
+
+        /* get tags from data-tags attribute */
+
+
+        const articleTag = article.getAttribute('data-tags');
+        console.log('to jest article selector data-tags', articleTag);
+
+
+        /* split tags into array */
+
+        const articleTagsArray = articleTag.split(' ');
+
+        /* for each tag */
+        for (let tag of articleTagsArray) {
+            console.log(tag);
+
+        }
+
+
+
+
+        /* generate HTML of the link */
+        /* START LOOP: for each tag */
+
+        /* generate HTML of the link */
+
+        /* add generated code to html variable */
+
+        /* END LOOP: for each tag */
+
+        /* insert HTML of all the links into the tags wrapper */
+
+        /* END LOOP: for every article: */
+    }
+}
+
+generateTags();
